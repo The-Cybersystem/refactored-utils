7ab29bbf0e15b307560d4e27a397848e7937d4db
The-Cybersystem <riven.thefoundation@proton.me>
2025-12-19 13:18:26 -0600

    Added project framework.

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    apps: [{
+    name: '',  // Custom name for your bot
+    script: 'main.py',     // Path to your main Python script
+         interpreter: '',  // Full path to your custom Python interpreter
+         cwd: '',  // Absolute path to the bot's working directory
+         env: {
+           NODE_ENV: 'development'  // Add any env vars your bot needs, like Discord token
+         },
+         instances: 1,  // Run one instance (bots usually don't need clustering)
+         autorestart: true,  // Auto-restart on crashes
+         watch: false  // Set to true if you want PM2 to restart on file changes (optional)
+       }]
+     };
