d64c949d9fc134d4fbbe3cf17e38ccf671269d35
The-Cybersystem <riven.thefoundation@proton.me>
2025-12-20 13:08:14 -0600

    Updated container with validator, added validator.py to utilities.

--- src/core/container.py
+++ src/core/container.py
@@ -2,6 +2,7 @@ from discord.ext import commands
 import discord
 import logging
 from sys import stdout
+from src.utils.validator import Validator
 
 
 class Container:
@@ -10,24 +11,36 @@ class Container:
             level=logging.INFO,
             format="%(asctime)s [%(levelname)s] %(message)s",
             handlers=[
-                logging.FileHandler("container.log"),
+                logging.FileHandler("main.log"),
                 logging.StreamHandler(stdout),
             ],
         )
         self.__logger: logging.Logger = logging.getLogger(__name__)
 
         # Initialise core services
-        # self._validator = Validator()
+        self._validator = Validator()
         # self._security_service = SecurityService()
         # self._cache_service = CacheService()
 
         # Initialise database layer
         # self._repository = Repository()
 
-        # economy abstraction not implemented
+        # dicsord economy abstraction not implemented
 
         # Initialise bot and related services
         intents = discord.Intents.all()
         self.__logger.debug("Creating bot...\nIntents: " + str(intents))
         self._bot = commands.Bot(command_prefix="/", intents=intents)
         self.__logger.debug("Bot created successfully.")
+
+        # minecraft server service not implemented
+
+    @property
+    async def bot(self) -> commands.Bot:
+        """Provices access to the Discord bot instance."""
+        return self._bot
+
+    @property
+    async def validator(self) -> Validator:
+        """Provides access to the validator service."""
+        return self._validator
