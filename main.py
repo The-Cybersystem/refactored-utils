0a909a8419c4ee320fcb80e4bf597558268bc66b
The-Cybersystem <riven.thefoundation@proton.me>
2025-12-20 12:31:15 -0600

    Updated main.py, minor logging update, added partial container framework.

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
-from sys import stdout
+from sys import stdout, exit as sysexit
 import logging
+from utils.config import ConfigManager
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s [%(levelname)s] %(message)s",
-    handlers=[logging.FileHandler("bot.log"), logging.StreamHandler(stdout)],
-)
 
-logger = logging.getLogger(__name__)
+def main():
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s [%(levelname)s] %(message)s",
+        handlers=[logging.FileHandler("main.log"), logging.StreamHandler(stdout)],
+    )
+    try:
+        logger: logging.Logger = logging.getLogger(__name__)
 
-logger.info("Starting bot...")
+        logger.info("Loading configuration...")
+        config = ConfigManager()
 
-logger.debug("Retrieving bot token...")
-token = config.get("TOKEN")
+        logger.debug("Retrieving bot token...")
+        token = config.get("TOKEN")
+
+        if not token:
+            logger.error("Bot token not found in configuration.")
+            sysexit(1)
+
+        logger.info("Configuration loaded successfully.")
+    except Exception as e:
+        logger.error(f"An error occurred: {e}")
+        sysexit(1)
+
+
+if __name__ == "__main__":
+    main()
