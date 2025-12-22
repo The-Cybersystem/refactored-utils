55b9921b3ab696ee3e685a64481499a008d4beee
The-Cybersystem <riven.thefoundation@proton.me>
2025-12-21 22:04:00 -0600

    Added properties for container.py

--- src/core/container.py
+++ src/core/container.py
@@ -4,6 +4,7 @@ import logging
 from src.utils.validator import Validator
 from src.services.security_service import SecurityService
 from src.services.cache_service import CacheService
+from src.repositories.mongodb_repository import MongoDBRepository
 
 
 class Container:
@@ -14,7 +15,7 @@ class Container:
         self._cache_service = CacheService()
 
         # Initialise database layer
-        # self._repository = Repository()
+        self._repository = MongoDBRepository()
 
         # dicsord economy abstraction not implemented
 
@@ -30,6 +31,21 @@ class Container:
     def bot(self) -> commands.Bot:
         """Provices access to the Discord bot instance."""
         return self._bot
+
+    @property
+    def cache_service(self) -> CacheService:
+        """Provides access to the cache service."""
+        return self._cache_service
+
+    @property
+    def repository(self) -> MongoDBRepository:
+        """Provides access to the mongo database repository."""
+        return self._repository
+
+    @property
+    def security_service(self) -> SecurityService:
+        """Provides access to the security service."""
+        return self._security_service
 
     @property
     def validator(self) -> Validator:
